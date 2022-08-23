5311d59e39911fa6b80123bfc381f29dfa42f82b
thanh dat <david#gmail.com>
2022-08-23 23:52:53 +0700

    #

--- resources/views/0905b/details.blade.php
+++ resources/views/0905b/details.blade.php
@@ -137,15 +137,7 @@ https://templatemo.com/tm-546-sixteen-clothing
                 </div>
             </div>
           </div>
-          <div class="col-md-12">
-            <ul class="pages">
-              <li><a href="#">1</a></li>
-              <li class="active"><a href="#">2</a></li>
-              <li><a href="#">3</a></li>
-              <li><a href="#">4</a></li>
-              <li><a href="#"><i class="fa fa-angle-double-right"></i></a></li>
-            </ul>
-          </div>
+          
         </div>
       </div>
     </div>
